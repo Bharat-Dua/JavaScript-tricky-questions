05243140dff96330bed4211df6fffed8c59b3423
Bharat-Dua <bharatdua13@gmail.com>
2025-01-25 13:16:13 +0530

    solved tricky typeof problem

---
+++ tricky-02.js
@@ -0,0 +1,6 @@
+let y = 1;
+y = typeof x;
+
+console.log(typeof y); // string
+
+//* Bcoz x is not defined , so y is undefined, and typeof undefined is string bcoz typeof always returns the data type in string
