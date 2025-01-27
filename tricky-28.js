ebfadf218369f4caf37bc756141266c4933f6ec2
Bharat-Dua <bharatdua13@gmail.com>
2025-01-27 15:28:50 +0530

    solved tricky promise problem

---
+++ tricky-28.js
@@ -0,0 +1,13 @@
+async function foo() {
+  console.log("A");
+  await Promise.resolve();
+  console.log("B");
+  await new Promise((resolve) => setTimeout(resolve, 0));
+  console.log("C");
+}
+
+console.log("D");
+foo();
+console.log("E");
+
+//* output order :- D A E B C
